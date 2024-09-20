b98fb10ee4e5eb19719778bb78817de30b3d98db
NaiPoneAi <zmin07@gmail.com>
2024-09-20 18:09:51 +0630

    first commit

---
+++ rest-application/src/test/java/com/zmo/rest_application/RestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zmo.rest_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
